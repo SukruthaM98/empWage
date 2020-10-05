45204471de1f61ead8bfce85dacf6897d822ccba
SukruthaM98 <msukrutha@gmail.com>
2020-10-05 17:15:30 +0530

    employee attendance calculated

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x 
 
 echo "welcome to employee wage computation"
+
+attendance=$((RANDOM%2))
+if [ $attendance -eq 0 ]
+then
+	echo "employee is present"
+else
+	echo"employee is absent"
+fi
