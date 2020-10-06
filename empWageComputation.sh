e20a5556a43a26c276342b90eeb9d943796ad71e
SukruthaM98 <msukrutha@gmail.com>
2020-10-06 08:58:54 +0530

    employee wage calculated using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,41 +7,21 @@ if [ $attendance -eq 1 ]
 then
 	echo "employee is present"
 else
-	echo"employee is absent"
+	echo "employee is absent"
 fi
 
-EmployeeFullTimeHours=8
-EmployeePartTimeHours=4
-EmployeeWagePerHr=20
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3));
 
-if [ $attendance -eq 1 ]
-then
-	salary=$(($EmployeeFullTimeHours*$EmployeeWagePerHr));
-	echo "Employee FullTime salary :"$salary
-else
-	salary=0
-	echo " Employee is absent"
-fi
+case $empCheck in
+	$isFullTime)
+		empHrs=8 ;;
+	$isPartTime)
+		empHrs=4;;
+	*)
+		empHrs=0 ;;
+esac
 
-if [ $attendance -eq 1 ]
-then
-	isFullTime=1
-	isPartTime=2
-	employeeCheck=$((RANDOM%3))
-
-	if [ $employeeCheck -eq $isFullTime ]
-	then
-		salary=$(( $EmployeeFullTimeHours*$EmployeeWagePerHr ))
-		echo " FullTime employee salary is :" $salary
-	elif [ $employeeCheck -eq $isPartTime ]
-	then
-		salary=$(( $EmployeePartTimeHours*$EmployeeWagePerHr ))
-		echo " PartTime employee salary is: " $salary
-	else
-		salary=0
-		echo "Employee is absent"
-	fi
-else
-	salary=0
-	echo " employee is absent"
-fi
+salary=$(($empHrs*$empRatePerHr));
